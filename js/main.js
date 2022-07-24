36a0eb092264b354778795ebf754f78caaffc94c
Mila Rut <mila.balashova2312@gmail.com>
2022-07-25 02:46:53 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {similarAds} from './data.js';
 import {generatePopup} from './popup.js';
+import {disableForm, enableForm} from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const ads = similarAds();
 const popup = generatePopup(ads[0]);
 
 mapCanvas.appendChild(popup);
+disableForm();
+enableForm();
